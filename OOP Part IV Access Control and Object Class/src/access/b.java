39f880dbebe09b00306193da5d989d412783e8d1
Adityapapal <papaladitya@gmail.com>
2024-06-04 14:49:38 +0530

    OOP Part IV

---
+++ OOP Part IV Access Control and Object Class/src/access/b.java
@@ -0,0 +1,15 @@
+package access;
+
+public class b extends a {
+
+        b(int num1, int num2, int num3) {
+            super(num1, num2, num3);
+        }
+
+    void getData (){
+        System.out.println("form class b Num1: " + num1);
+        System.out.println("From class b Num3: " + num3);
+    }
+
+
+}
