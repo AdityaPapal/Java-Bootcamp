df68dc5b42b6e274e9cc4d9b29c3bbcba367daea
Adityapapal <papaladitya@gmail.com>
2024-06-06 11:52:01 +0530

    collection

---
+++ Advanced Java/src/Collection/Main.java
@@ -0,0 +1,21 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Array
+        List<String> list = new ArrayList<>();
+        list.add("Mango");
+        list.add("Peru");
+        list.add("Apple");
+        list.add("Chiku");
+        System.out.println(list);
+        System.out.println();
+    }
+
+}
